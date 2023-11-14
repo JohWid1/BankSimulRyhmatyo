7aeefd89a522fc141ecb69a3c348cdc77de1772f
Jarno Tauriainen <t2taja00@students.oamk.fi>
2023-11-14 12:11:53 +0200

    transaction.js ja transaction_model luotu

---
+++ backend/models/transaction_model.js
@@ -0,0 +1,26 @@
+const db = require('../database');
+
+const transaction = {
+  getAll: function(callback) {
+    return db.query('select * from transaction', callback);
+  },
+  getById: function(id, callback) {
+    return db.query('select * from transaction where id_transaction=?', [id], callback);
+  },
+  add: function(transaction, callback) {
+    return db.query(
+      'insert into transaction (action,sum) values(?,?)',
+      [transaction.action, transaction.sum, transaction],callback);
+  },
+  delete: function(id, callback) {
+    return db.query('delete from transaction where id_transaction=?', [id], callback);
+  },
+  update: function(id, transaction, callback) {
+    return db.query(
+      'update transaction set action=?,sum=? where id_transaction=?',
+      [transaction.action, transaction.sum],
+      callback
+    );
+  }
+};
+module.exports = transaction;
