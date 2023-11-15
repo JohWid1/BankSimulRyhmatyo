124d942ce42ac74d7453142b99e2ec1fcbaa3097
Jarno Tauriainen <t2taja00@students.oamk.fi>
2023-11-15 16:45:57 +0200

    transaction valmis ja tietokanta sotkettu samassa branchissä :-/

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -5,22 +5,23 @@ const transaction = {
     return db.query('select * from transaction', callback);
   },
   getById: function(id, callback) {
-    return db.query('select * from transaction where id_transaction=?', [id], callback);
+    return db.query('select * from transaction where idtransaction=?', [id], callback);
   },
   add: function(transaction, callback) {
     return db.query(
-      'insert into transaction (action,sum,account_idaccount,cardused) values(?,?,?,?)',
-      [transaction.action, transaction.sum, transaction.account, transaction.cardused],
+      'insert into transaction (action, sum, cardaccountid) values(?,?,(select idcardhasaccount from card_has_account where card_idcard=? and account_idaccount=?))',
+      [transaction.action, transaction.sum, transaction.cardused, transaction.accountused],
       callback
+      
     );
   },
   delete: function(id, callback) {
-    return db.query('delete from transaction where id_transaction=?', [id], callback);
+    return db.query('delete from transaction where idtransaction=?', [id], callback);
   },
   update: function(id, transaction, callback) {
     return db.query(
-      'update transaction set action=?,sum=? where id_transaction=?',
-      [transaction.action, transaction.sum],
+      'update transaction set action=?,sum=? where idtransaction=?',
+      [transaction.action, transaction.sum, id],
       callback
     );
   }
