f013281b440f5a63a38f54b87d84e2363b68f391
Partsa31 <late.partinen@gmail.com>
2023-11-20 18:02:00 +0200

    login toimii

--- backend/app.js
+++ backend/app.js
@@ -27,4 +27,22 @@ app.use ('/transaction', transactionRouter);
 app.use('/card_has_account', card_has_accountRouter);
 app.use('/login', loginRouter);
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+  }
+
 module.exports = app;
