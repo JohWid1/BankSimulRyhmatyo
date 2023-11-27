fcb30c9911a3006077b07dd4b17f385dc88da1b4
Jarno Tauriainen <145262384+Jtauri@users.noreply.github.com>
2023-11-27 11:59:12 +0200

    Delete backend/database.js

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: '127.0.0.1',
-  port: '3307',
-  user: 'bankuser',
-  password: 'bankpass',
-  database: 'bank_simul_r14'
-});
-module.exports = connection;
