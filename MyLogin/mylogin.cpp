d49dccf7b656083e23c4bbcfbfc89b4bbc39c9e2
Partsa31 <late.partinen@gmail.com>
2023-12-04 15:31:50 +0200

    Versio

---
+++ MyLogin/mylogin.cpp
@@ -0,0 +1,3 @@
+#include "mylogin.h"
+
+MyLogin::MyLogin() {}
---
+++ MyLogin/mylogin.h
@@ -0,0 +1,12 @@
+#ifndef MYLOGIN_H
+#define MYLOGIN_H
+
+#include "MyLogin_global.h"
+
+class MYLOGIN_EXPORT MyLogin
+{
+public:
+    MyLogin();
+};
+
+#endif // MYLOGIN_H
